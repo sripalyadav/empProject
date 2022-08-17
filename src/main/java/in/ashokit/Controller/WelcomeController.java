a109fceeb4853a59d3d06e8e6d42f20ace5a7ddf
Sripalyadav <kookatlasripalyadav@gmail.com>
2022-08-17 14:01:14 +0530

    hi

---
+++ src/main/java/in/ashokit/Controller/WelcomeController.java
@@ -0,0 +1,41 @@
+package in.ashokit.Controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class WelcomeController {
+	
+	@RequestMapping("/welcome")
+	public String welcomeMsg_display(Model model) {
+		
+		String str = "welcome mr sripal yadav...!";
+		model.addAttribute("msg", str);
+		
+		
+		
+	/*	ModelAndView model = new ModelAndView();
+		
+		
+		model.addObject("key", "WELCOME TO THE SPRING MVC...!");
+		model.setViewName("index");*/
+		return "index";
+		
+		
+		
+	}
+
+	 @GetMapping("/wish")
+	public String wishMsg(Model model) {
+		
+		 model.addAttribute("msg","good morining sripal Yadav");
+		
+		
+		
+		return"index";
+	}
+	
+}
